f3d73473b780616c28cc1ece226739faa9a3d74c
Steve Fanega II <steve.fanega.ii@gmail.com>
2020-12-17 08:10:53 -0600

    Initial Commit

---
+++ public/python/SimpleScript.py
@@ -0,0 +1,5 @@
+#! /bin/python3
+
+import numpy as num
+
+print("Hello World!")
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- Start of Footer -->
+<div class="container-fluid bg-dark py-5">
+    <div class="container">
+        <p class="text-muted text-white">&copy; Fanega Web Development Copyright 2020</p>
+        <p class="text-muted">Powered by Laravel + Vue.js</p>    
+    </div>
+</div>
+<!-- End of Footer -->
